0c61e001011fc059d09089454b15940923e9902e
Alex Trottier <postables@rtradetechnologies.com>
2019-12-28 23:20:31 -0800

    chore: remove extra file

--- cmd/gateway/s3x/gateway-s3x-utils.go
+++ cmd/gateway/s3x/gateway-s3x-utils.go
@@ -109,3 +109,28 @@ func (x *xObjects) getMinioObjectInfo(ctx context.Context, bucketName, objectNam
 		UserDefined: obj.GetObjectInfo().UserDefined,
 	}, nil
 }
+
+func (x *xObjects) getObjectData(
+	ctx context.Context,
+	obj *Object,
+) ([]byte, error) {
+	resp, err := x.dagClient.DagGet(ctx, &pb.DagGetRequest{
+		Hash: obj.DataHash,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.GetRawData(), nil
+}
+
+// store the objectData on ipfs and returns its hash
+func (x *xObjects) storeObjectData(
+	ctx context.Context,
+	data []byte,
+) (string, error) {
+	resp, err := x.dagClient.DagPut(ctx, &pb.DagPutRequest{Data: data})
+	if err != nil {
+		return "", err
+	}
+	return resp.GetHashes()[0], nil
+}
