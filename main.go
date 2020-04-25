14449fe4107533362eb9b8f7cce0d6ada6a24523
george xie <george@xiegeo.com>
2020-04-25 11:13:08 +0800

    small changes to start a pull request

--- main.go
+++ main.go
@@ -1,22 +1,6 @@
 // +build go1.13
 
 /*
- * MinIO Cloud Storage, (C) 2016, 2017, 2018 MinIO, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
  * Below main package has canonical imports for 'go get' and 'go build'
  * to work with all other clones of github.com/RTradeLtd/s3x repository. For
  * more information refer https://golang.org/doc/go1.4#canonicalimports
@@ -30,7 +14,7 @@ import (
 	minio "github.com/RTradeLtd/s3x/cmd"
 
 	// Import gateway
-	_ "github.com/RTradeLtd/s3x/cmd/gateway"
+	_ "github.com/RTradeLtd/s3x/gateway"
 )
 
 func main() {
