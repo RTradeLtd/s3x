756d8f97c8bad30448bf6a9f5d77126954510511
george xie <george@xiegeo.com>
2020-05-04 21:42:41 +0800

    fix for golint

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,7 +1,7 @@
-// s3x/gateway imports s3x gateway only.
+// Package gateway imports s3x gateway only.
 // To enable all minio gateways please also import "github.com/minio/minio/cmd/gateway".
 package gateway
 
 import (
-	_ "github.com/RTradeLtd/s3x/gateway/s3x"
+	_ "github.com/RTradeLtd/s3x/gateway/s3x" //register the s3x gateway
 )
